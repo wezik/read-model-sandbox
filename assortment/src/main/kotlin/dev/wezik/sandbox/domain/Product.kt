d4721bbbbf8adaf795e9237016c208dc41949ef1
Dominik <wezik@users.noreply.github.com>
2026-04-01 01:02:37 +0200

    Expand product model

--- assortment/src/main/kotlin/dev/wezik/sandbox/domain/Product.kt
+++ assortment/src/main/kotlin/dev/wezik/sandbox/domain/Product.kt
@@ -4,14 +4,24 @@ import java.util.*
 
 data class Product(
   val id: UUID,
+  val category: Category,
   val styles: List<Style>,
 )
 
 data class Style(
   val id: UUID,
+  val color: String,
   val articles: List<Article>,
 )
 
 data class Article(
+  val size: String,
   val id: UUID,
 )
+
+enum class Category {
+  CAP,
+  DRESS,
+  JEANS,
+  T_SHIRT,
+}
--- assortment/src/main/kotlin/dev/wezik/sandbox/infrastructure/RandomProductRepository.kt
+++ assortment/src/main/kotlin/dev/wezik/sandbox/infrastructure/RandomProductRepository.kt
@@ -4,6 +4,7 @@ import dev.wezik.sandbox.domain.Article
 import dev.wezik.sandbox.domain.Product
 import dev.wezik.sandbox.domain.ProductRepository
 import dev.wezik.sandbox.domain.Style
+import dev.wezik.sandbox.domain.Category
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -13,10 +14,27 @@ class RandomProductRepository : ProductRepository {
   override suspend fun list() = (1..5).map { randomProduct() }
 
   private fun randomProduct(styleCount: Int = 5) =
-    Product(UUID.randomUUID(), (1..styleCount).map { randomStyle(5) })
+    Product(
+      id = UUID.randomUUID(),
+      styles = (1..styleCount).map { randomStyle(5) },
+      category = randomCategory(),
+    )
+
+  private fun randomCategory() = listOf(Category.CAP, Category.DRESS, Category.JEANS, Category.T_SHIRT).random()
 
   private fun randomStyle(articleCount: Int = 5) =
-    Style(UUID.randomUUID(), (1..articleCount).map { Article(UUID.randomUUID()) })
+    Style(
+      id = UUID.randomUUID(),
+      articles = (1..articleCount).map { randomArticle() },
+      color = randomColor(),
+    )
 
-  private fun randomArticle() = Article(UUID.randomUUID())
+  private fun randomColor() = listOf("black", "white", "red", "blue").random()
+
+  private fun randomArticle() = Article(
+    id = UUID.randomUUID(),
+    size = randomSize(),
+  )
+
+  private fun randomSize() = listOf("S", "M", "L", "XL").random()
 }
